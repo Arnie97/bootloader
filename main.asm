5b27892e40899cab9984c618a9e67db9b898cde0
Arnie97 <arnie97@gmail.com>
2015-12-31 13:03:13 +0800

    Show a colored border.

--- main.asm
+++ main.asm
@@ -1,19 +1,15 @@
 bits 16             ; runs in 16 bits real mode
 org 7C00h           ; loaded by INT 19h at address 7C00h
 
-start:
-    call clear_screen
-    mov ax, cs
-    mov ds, ax
+main:
+    call init
 
-    mov si, msg     ; loads the address of first byte of string
-    mov dh, 0       ; top row
-    mov dl, 0       ; left column
+    mov dh, 1       ; the second row
+    mov dl, 36      ; column
+    mov si, title   ; loads the pointer to string
     call print
 
 print:
-    push ax
-    cld
     call set_position
 
 next:
@@ -33,29 +29,45 @@ printchar:
     int 10h
     ret
 
-clear_screen:
+init:
+    cli             ; clear and disable interrupts
+    mov ax, cs      ; store data from code segment to
+    mov ds, ax      ;   data segment,
+    mov es, ax      ;   extra segment, and
+    mov ss, ax      ;   stack segment.
+    sti             ; then re-enable external interrupts
+
+    mov cl, 0       ; left
+    mov ch, 0       ; top
+    mov dl, 79      ; right
+    mov dh, 24      ; bottom
+    mov bh, 0E0h    ; colors
+    call set_color
+
+    mov cl, 4       ; left
+    mov ch, 3       ; top
+    mov dl, 75      ; right
+    mov dh, 22      ; bottom
+    mov bh, 0Ch     ; colors
+    call set_color
+
+    ret
+
+set_color:
     mov ah, 07h     ; scroll down window function
     mov al, 0       ; lines to scroll: scroll whole window
-
-    mov bh, 0Ah     ; background / foreground colors
-
-    mov cl, 0       ; left column number
-    mov ch, 0       ; upper row number
-    mov dl, 79      ; right column number
-    mov dh, 24      ; lower row number
-
     int 10h
     ret
 
 set_position:
-    mov ah, 0Fh     ; get current video mode and video page.
-    int 10h         ; bh will ne the video page currently being displayed
+    mov ah, 0Fh     ; Get current video mode and video page.
+    int 10h         ; BH will ne the video page currently being displayed
 
-    mov ah, 02h
-    int 10h         ; move
+    mov ah, 02h     ; move cursor
+    int 10h
     ret
 
-msg: db 'Hello World!', 0
+title:  db 'WARNING', 0
 
 ; $  is the address of current line
 ; $$ is the address of first instruction
